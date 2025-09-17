bfc9c8ef54fda93d0d8b24bbdf7f7823e350e4dd
Dalton Kajander <daltonkajander@yahoo.com>
2025-09-17 12:21:55 -0400

    Main funcationality

--- card_api.py
+++ card_api.py
@@ -5,51 +5,40 @@ import requests
 
 API_CALL_TIMEOUT_MS = 100 # Must be between 50-100 ms 
 API_URL = "https://api.scryfall.com"
-HEADER = { "Content-Type": "application/json" }
 
 foiling_to_price = {
     "nonfoil": "usd",
     "foil": "usd_foil",
     "etched": "usd_etched"
 }
-
 foil_options = ["nonfoil", "foil", "etched"]
 
-
-@dataclass(init=False, order=True)
+@dataclass(order=True)
 class Card:
-    # Each card has a name, collector number, set, foiling. These affect the hash of the card itself
-    # They also have a quanitity, but that is for the user
-    sort_index: int = field(init=False, repr=False)
+    sort_index: str = field(init=False, repr=False)
     name: str
     collector_number: str 
     set: str
-    foiling: str # No, Yes, and Etched
+    foiling: str
     quantity: int = 1
     price: float = 0.0
-    
-    def __init__(self, name: str, collector_number: str, set: str, foiling: str, quantity: int = 0, call_api: bool = False):
-        self.name = name
-        self.collector_number = collector_number
-        self.set = set
-        self.foiling = foiling if foiling in foil_options else "nonfoil" # Assume nonfoil is bad foiling option
-        
-        if (call_api): 
+    call_api: bool = False
+
+    def __post_init__(self):
+        object.__setattr__(self, "sort_index", self.name)
+        if (self.foiling not in foil_options): self.foiling = "nonfoil"
+        if (self.call_api):
             self.response_json = get_api_response(self)
             self.set_price_from_api()
         else: self.response_json = {}
-        
-    def __post_init__(self): object.__setattr__(self, "sort_index", self.name)
     
-    def __str__(self) -> str: 
-        return f"x{self.quantity} {self.name} [#{self.collector_number} {self.set}, {self.foiling}] = ${self.price}"
+    def __str__(self) -> str:  return f"x{self.quantity} {self.name} [#{self.collector_number} {self.set}, {self.foiling}] = ${self.price:.2f}"
     
     def generate_hash(self) -> str:
         return sha256(f"{self.name}{self.collector_number}{self.set}{self.foiling}".encode()).hexdigest()
     
     def set_price_from_api(self) -> None:
-        if (self.response_json == {}): 
-            print("Empty API response, calling the API")
+        if (self.response_json == {}):
             self.response_json = get_api_response(self)
         
         prices = self.response_json["prices"]
@@ -58,7 +47,7 @@ class Card:
         # Bad price handling
         if (price == None and self.foiling != "nonfoiil"): price = prices["usd"] # Use nonfoil price, if it exists
         if (price == None): self.price = 0.0 # No price, set to 0
-        else: self.price = price
+        else: self.price = float(price)
        
 class BadCardCallError(Exception):
     def __init__(self, code: int, card: Card) -> None:
@@ -69,18 +58,13 @@ class BadCardCallError(Exception):
     def __str__(self) -> str:
         return f"Got status code {self.code} for {self.card.name} [{self.card.collector_number}, {self.card.set}, {self.card.foiling}]"
 
-
 def get_api_response(card) -> dict:
     card_url = f"{API_URL}/cards/{card.set}/{card.collector_number}"
     response = requests.get(card_url)
     sleep(API_CALL_TIMEOUT_MS / 1000)
+    
+    if (response.status_code != 200):
+        print(card_url)
+        raise BadCardCallError(response.status_code, card)
+    
     return response.json()
-
-if __name__ == "__main__":
-    print("Welcome to cards.py")
-    card1 = Card("Alms Collector", "10", "CMM", "nonfoil", call_api = True)
-    card2 = Card("Alms Collector", "10", "CMM", "foil", call_api = True)
-    card3 = Card("Alms Collector", "455", "CMM", "etched", call_api = True)
-    print(card1)
-    print(card2)
-    print(card3)
--- main.py
+++ main.py
@@ -1,9 +1,89 @@
-import card_api
+import card_api, pyodbc
+from os import getcwd
+from datetime import datetime
 
-card1 = card_api.Card("Chromanticore", "144", "BNG", "nonfoil", call_api = True)
-card2 = card_api.Card("Breeding Pool", "399", "RVR", "foil", call_api = True)
-card3 = card_api.Card("Hunting Cheetah", "134", "MB2", "nonfoil", call_api = True)
+def get_cards_from_database(filename: str, sql: str = "", autocall_api: bool = False) -> list[card_api.Card]:
+    if (sql == ""): sql = "SELECT * FROM Cards"
+    
+    # Connect to database
+    try:
+        encoding = "latin-1"
+        directory = getcwd() + "\\"
+        driverStr = r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='
+        cnxn = pyodbc.connect(driverStr + directory + filename + ";")
+        cursor = cnxn.cursor()
+        cnxn.setdecoding(pyodbc.SQL_CHAR, encoding=encoding)
+        cnxn.setdecoding(pyodbc.SQL_WCHAR, encoding=encoding)
+        cnxn.setencoding(encoding=encoding)
+        print(f"Opened {filename}")
+    except Exception as err:
+        print(err)
+        return []
+    
+    # Read the database
+    
+    try: cursor.execute(sql)
+    except Exception as err: cursor.execute("SELECT * FROM Cards")
+    
+    rows = cursor.fetchall()
+    card_list = []
+    
+    # Create the card objects
+    for temp_card in rows:
+        card_name = temp_card[0]
+        card_cn = temp_card[1]
+        card_set = temp_card[2]
+        card_foil = temp_card[3].lower()
+        card_quantity = int(temp_card[4])
+        card = card_api.Card(card_name, card_cn, card_set, card_foil, quantity = card_quantity, call_api = autocall_api)
+        card_list.append(card)
+        
+    
+    # Clean up
+    cursor.close()
+    cnxn.close()
+    
+    return card_list
 
-print(card1)
-print(card2)
-print(card3)
+def get_card_prices(cards: list[card_api.Card], check_cache: bool = True, write_to_cache: bool = True) -> None:
+    cache_filename = "prices.cache"
+    old_cache = {}
+    new_cache = {}
+    today = datetime.today().strftime("%Y%m%d")
+    
+    if (check_cache):
+        with open(cache_filename, "r") as file:
+            # If date is today, we can continue
+            cache_date = file.readline().strip()
+            print(f"{cache_date = } {today = }")
+            if (today == cache_date): # We are good to continue
+                print("We are checking the old cache now")
+                prices = [l.strip().split(",") for l in file.readlines()]
+                for price in prices:
+                    old_cache[price[0]] = price[1]
+                    
+    # Go through old_cache and get prices
+    # If check_cache = False, then old_cache is empty no matter what
+    for card in cards:
+        card_hash = card.generate_hash()
+        if (card_hash in old_cache):
+            card.price = float(old_cache[card_hash])
+        
+        else:
+            card.set_price_from_api()
+            new_price = card.price
+            new_cache[card_hash] = new_price
+            # This is where we call the API
+            # We will also write the new price to the new_cache, only writing to file if allowed
+    
+    # Write all the new prices to the cache
+    if (write_to_cache):
+        print("We are now writing the new cache")
+        with open(cache_filename, "w") as file:
+            file.write(f"{today}\n")
+            for hash_key in new_cache:
+                file.write(f"{hash_key},{new_cache[hash_key]}\n")
+            
+
+x = get_cards_from_database("Magic.accdb", sql = "SELECT * FROM Cards WHERE Amount >= 2")
+get_card_prices(x, True, True)
